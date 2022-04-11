5826e23e1b4f1ecd3966f5c0c2200b58d03efdd9
Natanonkhuanata <101494144+Natanonkhuanata@users.noreply.github.com>
2022-04-11 14:24:09 +0700

    Create EP8.py

---
+++ EP8.py
@@ -0,0 +1,21 @@
+for i in range(1,2):
+    for num in range(1,6):
+        print(num,'',end='')
+
+    print()
+    for num in range(1,5):
+        print(num,'',end='')
+
+    print()
+    for num in range(1,4):
+        print(num,'',end='')
+
+    print()
+    for num in range(1,3):
+        print(num,'',end='')
+
+    print()
+    for num in range(1,2):
+        print(num,'',end='')
+
+    print()
