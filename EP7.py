53ea005fa569385c03c2dafc273635d055d0688f
Natanonkhuanata <101494144+Natanonkhuanata@users.noreply.github.com>
2022-04-11 14:24:12 +0700

    Create EP7.py

---
+++ EP7.py
@@ -0,0 +1,19 @@
+for n in range(1,2):
+        for i in range(1,n + 1):
+            print('    ',i ,end='')
+        
+        print()
+        for i in range(n+1,0,-1):
+            print('',i,' ',end='')
+        
+        print()
+        for i in range(n+2,0,-1):
+            print('', i,end='')
+        
+        print()
+        for i in range(n+3,0,-1):
+            print('',i,'',end='')
+
+        print()
+     
+    
